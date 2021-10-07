caaf40a5561d5832db0f3b2635d5e505d61db97a
colin mcgovern <colinmcg@udel.edu>
2021-10-07 15:01:15 -0400

    stock_react

--- stock_react/server.py
+++ stock_react/server.py
@@ -11,15 +11,15 @@ api = Api(app)
 
 class APPL(Resource):
     def get(self):
-        
+
         if (Path("appl.json").exists()==False):
-            reply = requests.get("https://api.twelvedata.com/time_series?symbol=AAPL&interval=1day&outputsize=5000&apikey=2567314b3b4b41328b4083fec69d0f89")
+            reply = requests.get("https://api.twelvedata.com/time_series?symbol=AAPL&interval=1day&outputsize=5000&apikey={}".format(apikey))
             # reply = requests.get("https://www.breakingbadapi.com/api/characters/1")
             print("download")
             file = open("appl.json","w")
             file.write(reply.text)
             file.close()
-        
+
         # data = data.to_dict('records')
         # return {'data' : data}, 200
 
